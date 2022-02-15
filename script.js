3b3aeaddc0e3b7aa1e1d9b37e8f4efb28f5b71d9
unknown <mbrector@gmail.com>
2022-02-15 14:22:24 -0500

    image fixes and button work

--- script.js
+++ script.js
@@ -9,37 +9,53 @@ class Player extends Shooter{
     constructor(accuracy){
         super(accuracy)
         this.accuracy = (Math.floor(Math.random()*10)+65)/100 //player generates an accuracy between 65-75%
+        this.score = 0
+        this.madeShot = true
     }
 }
-//computer class extending player class
+//computer class extending shooter class
 class Computer extends Shooter{
     constructor(accuracy){
         super(accuracy)
         this.accuracy = (Math.floor(Math.random()*20)+60)/100 //computer generates an accuracy between 60-80%
+        this.score = 0
+        this.madeShot = true
     }
 }
 //compare player and computer accuracy vs a Math.random to see if they score
 let kobe = new Player
 let shaq = new Computer
 
+// let madeShot = null
 if (kobe.accuracy > Math.random()){
-
-
+        kobe.madeShot = true;
+        kobe.score++
+    }
+        else {kobe.madeShot = false
+}
+if (shaq.accuracy > Math.random()){
+    shaq.madeShot = true;
+    shaq.score++
+}
+    else {shaq.madeShot = false
 }
 
-console.log(kobe.accuracy)
-console.log(shaq.accuracy)
+console.log(kobe.madeShot)
+console.log(shaq.madeShot)
+console.log(kobe.score)
+console.log(shaq.score)
 
 
 //way to generate picture changes to denote a hit or miss
+
 let myImage = document.querySelector('#playerHoop');
 
 myImage.onclick = function() {
-  let mySrc = myImage.getAttribute('src');
-  if(mySrc === 'images/emptyHoop.jpg') {
-    myImage.setAttribute ('src','images/swish.jpg');
-  } else if(mySrc === 'images/swish.jpg') {
-    myImage.setAttribute ('src','images/doink.jpg')
+  let mySrc = myImage.getAttribute('src');                                          //function for player and function for computer tied to button
+  if(mySrc === 'images/emptyHoop.jpg' && kobe.madeShot == true) {                      //logic for scoreboard to update
+    myImage.setAttribute ('src','images/swish.jpg');                                       //set timeout to go back to emptyHoop
+  } else if(mySrc === 'images/emptyHoop.jpg' && kobe.madeShot == false) {                  //wait for another click
+    myImage.setAttribute ('src','images/doink.jpg')                                       //
   }
     else {
         myImage.setAttribute ('src','images/emptyHoop.jpg') 
