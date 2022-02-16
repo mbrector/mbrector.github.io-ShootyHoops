7696ead14d722cf5b97ccab122e11b305dd23e32
unknown <mbrector@gmail.com>
2022-02-15 20:33:13 -0500

    button works both shots consistently

--- script.js
+++ script.js
@@ -26,7 +26,6 @@ class Computer extends Shooter{
 let kobe = new Player
 let shaq = new Computer
 
-// let madeShot = null
 if (kobe.accuracy > Math.random()){
         kobe.madeShot = true;
         kobe.score++
@@ -34,8 +33,8 @@ if (kobe.accuracy > Math.random()){
         else {kobe.madeShot = false
 }
 if (shaq.accuracy > Math.random()){
-    shaq.madeShot = true;
-    shaq.score++
+        shaq.madeShot = true;
+        shaq.score++
 }
     else {shaq.madeShot = false
 }
@@ -50,11 +49,11 @@ console.log(shaq.score)
 
 let myImage = document.querySelector('#playerHoop');
 
-function kobeShot() {
+function playerShot() {
   let mySrc = myImage.getAttribute('src');                                          //function for player and function for computer tied to button
-  if(mySrc === 'images/emptyHoop.jpg' && kobe.madeShot == true) {                      //logic for scoreboard to update
+  if(mySrc === 'images/emptyHoop.jpg' && kobe.madeShot === true) {                      //logic for scoreboard to update
     myImage.setAttribute ('src','images/swish.jpg');                                       //set timeout to go back to emptyHoop
-  } else if(mySrc === 'images/emptyHoop.jpg' && kobe.madeShot == false) {                  //wait for another click
+  } else if(mySrc === 'images/emptyHoop.jpg' && kobe.madeShot === false) {                  //wait for another click
     myImage.setAttribute ('src','images/doink.jpg')                                       //
   }
     else {
@@ -64,17 +63,27 @@ function kobeShot() {
 
 let myImage2 = document.querySelector('#computerHoop');
 
-function shaqShot() {
+function computerShot() {
   let mySrc = myImage2.getAttribute('src');
-  if(mySrc === 'images/emptyHoop.jpg' && shaq.madeShot == true) {
+  if(mySrc === 'images/emptyHoop.jpg' && shaq.madeShot === true) {
     myImage2.setAttribute ('src','images/swish.jpg');
-  } else if(mySrc === 'images/swish.jpg' && shaq.madeShot == false) {
+  } else if(mySrc === 'images/emptyHoop.jpg' && shaq.madeShot === false) {
     myImage2.setAttribute ('src','images/doink.jpg')
   }
     else {
         myImage2.setAttribute ('src','images/emptyHoop.jpg') 
     }
 }
+
+// document.getElementById('shoot').addEventListener('click', function(){
+//     playerShot();
+//     computerShot();
+// });
+
+var btn = document.getElementById("shoot");
+     
+btn.addEventListener("click", playerShot);
+btn.addEventListener("click", computerShot);
 
 
 //scoreboard
