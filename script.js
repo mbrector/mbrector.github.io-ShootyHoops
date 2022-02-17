97580283a180be476ea4357863f700d9bb1bed78
unknown <mbrector@gmail.com>
2022-02-17 12:50:28 -0500

    alert work

--- script.js
+++ script.js
@@ -34,69 +34,71 @@ let myImage = document.querySelector('#playerHoop');
 let myImage2 = document.querySelector('#computerHoop');
 
 function playerShot() {
-  let mySrc1 = myImage.getAttribute('src');
-  if(kobe.score<10 && shaq.score<10){
-      if (kobe.accuracy > Math.random()){
-        kobe.madeShot = true;
+    let mySrc1 = myImage.getAttribute('src');
+    if(kobe.score<10 && shaq.score<10){
+        if (kobe.accuracy > Math.random()){
+            kobe.madeShot = true;
+        }
+        else {kobe.madeShot = false}                                                                                            
+        if(mySrc1 === 'images/emptyHoop.jpg' && kobe.madeShot === true) {                      
+            myImage.setAttribute ('src','images/swish.jpg');
+            kobe.score++                                                                            
+        } else if(mySrc1 === 'images/emptyHoop.jpg' && kobe.madeShot === false) {                  
+            myImage.setAttribute ('src','images/doink.jpg')                                      
+        }
+        if(kobe.score<10){document.querySelector('#homeScore').innerHTML = `0${kobe.score}`}
+        else{document.querySelector('#homeScore').innerHTML = `${kobe.score}`}
+
+        let mySrc2 = myImage2.getAttribute('src');
+        
+        if (shaq.accuracy > Math.random()){
+            shaq.madeShot = true;
+        }
+        else {shaq.madeShot = false}
+
+        if(mySrc2 === 'images/emptyHoop.jpg' && shaq.madeShot === true) {
+            myImage2.setAttribute ('src','images/swish.jpg');
+            shaq.score++
+        } 
+        else if(mySrc2 === 'images/emptyHoop.jpg' && shaq.madeShot === false) {
+            myImage2.setAttribute ('src','images/doink.jpg')
+        }
     }
-        else {kobe.madeShot = false
-    }                                                                                            
-    if(mySrc1 === 'images/emptyHoop.jpg' && kobe.madeShot === true) {                      
-        myImage.setAttribute ('src','images/swish.jpg');
-        kobe.score++                                                                            
-    } else if(mySrc1 === 'images/emptyHoop.jpg' && kobe.madeShot === false) {                  
-        myImage.setAttribute ('src','images/doink.jpg')                                      
+    if(shaq.score<10){
+        document.querySelector('#visitorScore').innerHTML = `0${shaq.score}`
     }
-    if(kobe.score<10){
-        document.querySelector('#homeScore').innerHTML = `0${kobe.score}`
-    }
-    
-    
     else{
-        document.querySelector('#homeScore').innerHTML = `${kobe.score}` 
+        document.querySelector('#visitorScore').innerHTML = `${shaq.score}` 
     }
-    let mySrc2 = myImage2.getAttribute('src');
-    
-  if (shaq.accuracy > Math.random()){
-    shaq.madeShot = true;
-}
-else {shaq.madeShot = false
-}
-if(mySrc2 === 'images/emptyHoop.jpg' && shaq.madeShot === true) {
-    myImage2.setAttribute ('src','images/swish.jpg');
-    shaq.score++
-  } else if(mySrc2 === 'images/emptyHoop.jpg' && shaq.madeShot === false) {
-    myImage2.setAttribute ('src','images/doink.jpg')
-  }
-}
-if(shaq.score<10){
-    document.querySelector('#visitorScore').innerHTML = `0${shaq.score}`
-}
-else{
-    document.querySelector('#visitorScore').innerHTML = `${shaq.score}` 
-}
-     setTimeout(function() {
+    setTimeout(function() {
         myImage.setAttribute ('src','images/emptyHoop.jpg') 
         myImage2.setAttribute ('src','images/emptyHoop.jpg') 
         
-    }, 500);
+    }, 750);
 }
 
-function win(){if(kobe.score>=10 && shaq.score<10){
-    alert('You win!\nHit "Reset" to play again.')
-}
-else if(shaq.score>=10 && kobe.score<10){
-    alert('You lose!\nHit "Reset" to play again.')
-}
-}
 
 var btn = document.getElementById("shoot");
-     
+
 btn.addEventListener("click", playerShot);
 btn.addEventListener("click", function(){
     setTimeout(win, 1000);
 })
 
+function win(){
+    if(kobe.score >= 10 && shaq.score < 10) {
+        btn.disabled = true
+        alert('You win!\nHit "Reset" to play again.')
+    }
+    else if(shaq.score >= 10 && kobe.score < 10){
+        btn.disabled = true
+        alert('You lose!\nHit "Reset" to play again.')
+    }
+    else if (shaq.score == 10 && kobe.score == 10) {
+        btn.disabled = true
+        alert('Tie game! Please try again!\nHit "Reset" to play again.')
+    }
+}
 
 //scoreboard
 
@@ -113,16 +115,17 @@ function reset(){
     document.querySelector('#visitorScore').innerHTML = `0${shaq.score}`;
     kobe = new Player;
     shaq = new Computer;
+    btn.disabled = false
 }
 resetGame.addEventListener("click", reset)
 
 
 //instruction button
 function instructions(){
-    alert("Welcome to Shooty Hoops!");
-    alert("It is you against the computer in a 1v1!\nFirst to 10 wins!")
-    alert("Press the Shoot button to get started.\nYou will both shoot at the same time.")
-    alert("Your accuracy is randomly generated between 65-75%\nThe computer accuracy is randomly generated between 60-80%")
+    alert("                                 Welcome to Shooty Hoops!");
+    alert("                         It is you against the computer in a 1v1!\n                                          First to 10 wins!")
+    alert("                         Press the Shoot button to get started.\n                         You will both shoot at the same time.")
+    alert("             Your accuracy is randomly generated between 65-75%\n        The computer accuracy is randomly generated between 60-80%")
 }
 var bttn = document.getElementById("instruct");
 
